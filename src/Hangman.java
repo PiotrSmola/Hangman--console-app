83936f2e8e772d5df1d492280f89a8bd41aaaf1e
Piotr Smoła <piotrsmola2002@gmail.com>
2023-07-14 18:53:27 +0200

    adding some messages and minor updates

--- src/Hangman.java
+++ src/Hangman.java
@@ -6,7 +6,7 @@ import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
-public class hangman {
+public class Hangman {
     private static final String API_URL = "https://random-word-api.herokuapp.com/word";
     private static final int MAX_TRIES = 10;
 
@@ -27,10 +27,13 @@ public class hangman {
 
         while (incorrectGuesses < MAX_TRIES) {
             String guess = readFromUser();
+            System.out.println("\nA word to guess: " + hiddenWord.toString());
+            System.out.print("Enter a letter or try to guess the whole word: ");
 
             if (guess.length() == 1) {
                 char letter = guess.charAt(0);
                 if (guessedLetters.contains(letter)) {
+                    System.out.println("This letter has already been given. Try again");
                     continue;
                 }
 
@@ -39,24 +42,31 @@ public class hangman {
                 if (word.contains(String.valueOf(letter))) {
                     update(word, hiddenWord, letter); //update hidden word
                     if (!hiddenWord.toString().contains("_")) {
+                        System.out.println("Congratulations! You guessed the word: " + hiddenWord.toString());
                         return;
                     }
                 } else {
                     incorrectGuesses++;
                     drawHangman(incorrectGuesses);
-
+                    System.out.println("Incorrect letter!");
+                    System.out.println("Attempts left: " + (MAX_TRIES - incorrectGuesses));
                 }
             } else if (guess.length() > 1) {
                 if (guess.equals(word)) {
+                    System.out.println("Congratulations! You guessed the word: " + word);
                     return;
                 } else {
                     incorrectGuesses++;
                     drawHangman(incorrectGuesses);
+                    System.out.println("Incorrect word!");
+                    System.out.println("Attempts left: " + (MAX_TRIES - incorrectGuesses));
 
                 }
             }
         }
 
+        System.out.println("\nYou lost! You failed to guess the word. The guessed word is:" + word);
+        System.out.println("\nThank you for playing");
     }
 
     private static String getWord() {
